254a2d38be6e5ef6aa2e21389c40394a642a2d4e
jana8964e <janaise.g@escolar.ifrn.edu.br>
2021-04-26 02:59:09 -0300

    outra tentativa

---
+++ application/views/listar_cardapio.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table>
+      <tr><td> Data </td><td> Descricao </td></tr>
+      <tr><td> 29/04/2021 </td><td> Creme de frango + suco </td></tr>
+      <tr><td> 30/04/2021 </td><td> Arroz + Feijão + Carne + suco </td></tr>
+    </table>
+
+</body>
+</html>
